fdab97d97809949ff8cd8affec8d108f86bfb997
Baxtiyor0101 <oqonboyevb@gmail.com>
2024-10-16 08:41:19 +0500

    operators

---
+++ lesson-4/app.js
@@ -0,0 +1,138 @@
+// Arifmetik operators + - / * % ++ --
+// let test = 12;
+// let ism = "Jasur";
+// let familya = "Botirov";
+// let fullname = ism + " " + familya;
+
+// console.log(test + 5); // 17
+// console.log(ism + " " + familya);
+
+// let str="";
+// console.log(typeof(str)); //
+
+// typeof => malumotlarning typeni aniqlab beradi
+// console.log("test" - "test");
+// console.log(12 - 3);
+// NaN = not a number
+
+// console.log(typeof NaN);
+
+// console.log("dd"+43);
+// console.log(typeof (33));
+
+// console.log(typeof("12"/"d"));
+// let test = Symbol("test");
+// console.log(typeof test);
+
+// console.log(2*"3s");
+
+// let num = 0;
+// console.log(num++);
+// console.log(um++n);
+// num--;
+// num--;
+// num--;
+// num--;
+// console.log(num--);
+// console.log(--num);
+// = == ===
+// console.log(false !== 0);
+// Comparision operators < > <= >= == === != !==
+// console.log(true===1);
+// console.log(2<3);
+// console.log(2>3);
+// console.log(1>=1);
+// console.log(1<=1);
+// console.log(1 == "1"); // equality
+// console.log("1" === "1"); // strict equality
+// console.log(2 !== "2"); // not strict equality
+// console.log("2" != 2); // not  equality
+// console.log(12 / 5);
+
+// kvadrat tomomin berilgan uning yuzi va pere toping
+// doiraning radiusi 5 uning yuzi va pere toping
+
+// let a = prompt("kvadrat tomoniin kiriting");
+// let s = a ** 2; // a*a;
+// let p = 4 * a; // 4*a;
+// console.log("yuzi " + s);
+// console.log("peremetri " + p);
+
+// let r = prompt("doira radiusini  kirit");
+// let s = 3.14 * r ** 2;
+// let p = 2 * 3.14 * r;
+
+// console.log("yuzi " + s);
+
+// console.log("peremetri " + p);
+
+// console.log(0**0);
+
+//////////////type Conversion > bir typedan ikkinchi typega otish
+// Number()
+// console.log(Number("2")); 2
+// console.log(Number(2));  2
+// console.log(Number(""));  // 0
+// console.log(Number(true)); // 1
+// console.log(Number(false)); //0
+// console.log(Number("aa")); //NaN
+// console.log(Number(null)); // 0
+// console.log(Number(undefined)); // NaN;
+// console.log(Number({})); // NaN;
+// console.log(Number([])); // 0
+// console.log(Number(0n)); //0
+
+// Boolean()
+// console.log(Boolean(true)); // true);
+
+// console.log(Boolean(0)); // false);
+
+// console.log(Boolean("")); // false);
+
+// console.log(Boolean(" ")); // true);
+
+// console.log(Boolean("0")); // true);
+
+// console.log(Boolean(null)); // false);
+
+// console.log(Boolean(undefined)); // false);
+
+// console.log(Boolean([])); // true);
+
+// console.log(Boolean({})); // true);
+// console.log(Boolean(3));
+// console.log(Boolean(0n));
+
+//  falsy values
+// 0
+// undefined
+// false
+// null
+// ""
+// 0n
+// NaN
+
+// truthy values ( falsy valuega kirmaydiag hamma qiymatlar )
+
+// ///////////////////  String()
+// console.log( String(undefined)); //
+// console.log(String(true)); //);/"true"
+// console.log(String(3));
+
+// Number mathods
+// parseFloat
+// parseInt
+// let num = "12km";
+// let res =parseFloat(num);
+// console.log(res);
+
+// let num = 12.324;
+// let res = parseInt(num);
+
+// console.log(res); // 12
+
+//  togri tortburchakning tomonlari berigan (promt yordamida oling va mda 12m)
+// uning yuzi va pere toping
+
+// let l = prompt("santimentda kiriting");
+// alert(parseFloat(l));
