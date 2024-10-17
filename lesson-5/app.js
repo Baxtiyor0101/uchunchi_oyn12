e5c9e72b81b159112da059722c5082fd05f6076f
Baxtiyor0101 <oqonboyevb@gmail.com>
2024-10-17 08:24:34 +0500

    logical

---
+++ lesson-5/app.js
@@ -0,0 +1,105 @@
+// Logical Operators => && || !
+// console.log(5 && 4 && 0 && false);
+// && birinchi falsy valueni qidiradi topolmasa oxirgi truthy valuni oladi
+
+// console.log(0 || true || "test");
+// || birinchi truthy valueni qidiradi topolmasa oxirgi falsy valuni oladi
+
+// console.log( 0 && 8 || false && "r");
+
+// && => *
+// || => +
+// ! =>  -
+
+// ////////////////////////  if
+
+// let age = 18;
+// if (age > 19) {
+//   console.log("You are an adult");
+// }
+// console.log("this is just a test");
+// console.log("this is just a test");
+// console.log("this is just a test");
+
+/////////////////if else
+// let age = 16;
+// if (age > 16) {
+//   console.log("You are an adult");
+// } else {
+//   console.log("You are a child");
+// }
+
+// //////////////// if , else if , else
+// let age = "aduljhgt";
+// if (age === "test") {
+//   console.log("You are a test");
+// } else if (age === "child") {
+//   console.log("You are a child");
+// } else if (age === "adult") {
+//   console.log("You are an adult");
+// } else {
+//   console.log("You are not defined");
+// }
+
+// let age = 77;
+// 16  dan kichik bolsa sen yoshsan
+// 16dan katta 25 dan kichik bolsa sen yoshmassan
+// 25 va 50 orasida bolsa sen odam bopqolding
+// 50dan katta bolsa sizzi yoshiz ulug
+
+// if (age < 16) {
+//   console.log("Sen yoshsan");
+// } else if (age >= 16 && age <= 25) {
+//   console.log("Sen yoshmassan");
+// } else if (age >= 26 && age <= 50) {
+//   console.log("Sen odam bopqolding");
+// } else {
+//   console.log("Sizzi yoshiz ulug");
+// }
+
+// 1 kabisa yini topish
+// 2 temperaturaga comment yozish 50 30  20 > 16 >10 >0
+// son kiritiladi u son ikkiga va uchga bolinishini tekshiring
+
+// let yil = +prompt("hohlagan yilingizni kiritng!");
+// if (yil % 4 == 0) {
+//   alert("kabisa yili");
+// } else{
+//     alert("kabisa yil emas");
+// }
+
+// //////////////  switch case
+// let meva = prompt("yoqtrgan mevangizni kiriting");
+// switch (meva) {
+//   case "olma":
+//     alert("mevaning narxi 1000");
+//     break;
+//   case "behi":
+//     alert("mevaning narxi 2000");
+//     break;
+//   case "nok":
+//     alert("mevaning narxi 3000");
+//     break;
+//   case "banana":
+//     alert("mevaning narxi 4000");
+//     break;
+//   default:
+//     alert("bunday meva yoq");
+//     break;
+// }
+
+
+// console.log("lkk");
+
+// Create a program that takes input from the user to choose a type of meal:
+
+// user 1dan 4gacha qiymat kiritadi 1 to 4:
+// 1: Breakfast
+// 2: Lunch
+// 3: Dinner
+// 4: Snack
+ 
+// agar bundan boshqa son bolsa bunday ovqatlanish vaqti yoq
+
+
+// ikkita son beriladi ularning birinchi kattasini keyin kichigini chiqarsin 
