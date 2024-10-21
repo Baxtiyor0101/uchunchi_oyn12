7a0e4b7fbd9b93717d0334602df561227cb18364
Baxtiyor0101 <oqonboyevb@gmail.com>
2024-10-21 09:58:09 +0500

    loop

---
+++ lesson-8/app.js
@@ -0,0 +1,66 @@
+/*
+push
+pop
+unshift
+shift boshidan ochirad
+cancat arraylarni qoshish uchun
+indexOf elemnt indexini topish uchun
+slice elmntlarni copy qilish
+splice elmntlarni ochiirsh qoshish (cut)
+reverse elmntlarni teskari qilad
+include bormi yoqmi
+findindex sharga togri keladigan birinchi index
+join array elmntlarini qoshib string qilib beradi
+lastindexof oxidan birinchi keladigan elmnt indexini topadi
+
+reduce array elemntlarini qoshib beradi
+some shartga mos keladiga elmnt bormi
+foreach har bir array elmntlari bilan action bajarish
+fild shatga mos keluchi birinchi elemnt
+map
+sort
+filter
+*/
+
+// let fruits = ["apple", "orange", "kiwi", "banana"];
+
+// let newarr = fruits.forEach((val,inx) => {
+//     console.log(val, inx);
+// });
+
+// console.log(fruits);
+
+// let arr = [12, 4, 6, 7, 8, 9, 10, 88, 32, 2, 5];
+// let sorted = arr.sort((a, b) => a - b);
+// console.log(sorted[sorted.length-1]);
+
+// let str = prompt("text kiriting");
+// let serchingText = prompt("qidirilayotgan sozni kirit");
+// let test = str.split(" ").filter((val, inx) => val === serchingText).length;
+// alert(test)
+
+////////////////////////////////////loop
+// for
+// while
+// do while
+// console.log(i);
+// for (let i = -10 ; i < 11; i--) {
+//     console.log(i);
+// }
+// i+=2  => i=i+2
+// i*=2 => i=i*2
+
+// while
+// let i = -10;
+// while (i < 10) {
+//   console.log(i);
+// //   i++;
+// }
+
+// do while
+// let i = -10;
+// do {
+//   console.log(ei);
+//   i++;
+  
+// } while (i < 10);
