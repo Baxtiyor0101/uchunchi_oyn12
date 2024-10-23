c3bb24612462b2bb32c9b7aec9ea9dee61afda53
Baxtiyor0101 <oqonboyevb@gmail.com>
2024-10-23 08:13:03 +0500

    lesson9

---
+++ lesson-9/app.js
@@ -0,0 +1,86 @@
+// Date
+// Math functions
+// Functions
+
+// let date = new Date();
+// let res = new Date("2005-01-01T05:04:30");
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getMonth());
+// console.log(date.getTime());
+// console.log(date.getSeconds());
+// console.log(date.getTimezoneOffset());
+// console.log(date.getUTCDate());
+// console.log(date.getUTCHours());
+// console.log(date.getUTCDay());
+// console.log(date.getUTCDay());
+// console.log(res);
+
+// Math functions
+// let num = 16;
+// console.log(Math.sqrt(num)); // ildizini(2) topadi
+// console.log(Math.abs(num));   // modulini faqat musbat chiqadi
+// console.log(Math.cbrt(num));  //cubic ildizini(3)
+// console.log(Math.pow(num, 2));  // luboy sonning luboy darajasini topib beradi
+// console.log(Math.LOG10E); //);
+// console.log(Math.sin(Math.PI/4)); // sinusini
+// console.log(Math.max(2, 4, 4, 5, 2, 22)); maximumini topadi sonlar orasidan
+// console.log(Math.abs(Math.min(2, 4, 4, 5, -2, 22))); minimumini sonlar oras
+
+// ParsInt
+// let num = 4.499;
+// console.log(Math.ceil(num));  // sonning tepadagi butun sonini chiqadi
+// console.log(Math.floor(num));  // faqat butunini olib beradi
+// console.log(Math.round(num));  // yaxlitlash 5 gacha bolsa pastga 5 va undan yuqori bilsa tepadagi butun sonni oladi
+
+//  random
+// console.log(Math.ceil(Math.random() * 3)); //
+
+// Function
+// bizda 3 xil function bor
+// 1 function decloration
+// 2 function expression
+// 3 arrow function
+
+// 1 function decloration
+// let num=34
+// function myFunc(a = 10, b = 20) {
+//   // console.log("hello function decloration",a,b);
+//   return a + b;
+// }
+// console.log(myFunc(12, 11));
+// console.log(myFunc(15));
+// myFunc(35);
+
+// ////////////////////// misollar yechish
+let arr = [1, 2, 3, 4, 5];
+// array ichidagi qiymatlar yigindisini topib beradigan funcfion tuzing 
+
+
+// aylananing radiusi berilgan bolsa uzunligini yuzini topib beruvchi function tuzing 
+
+// berilgan sonning factarialini topib beruvchi function tuzing 
+
+// uchta sonning eng kattasini topib beruvchi function tuzing 
+
+// berilgan stringing ichida nechta unli harf borligini topib beruvchi function tuzing 
+
+
+
+
+// Homework
+// user tugulgan kunini ismini promtda kiritadi 
+// javob {ism} siz {yil} {oy} {kun} {soat} {munut} {sekunt } yashagansiz dep chiqarishi kk
+
+
+// x va y oraligidagi ixtiyoriy sonni topib beruvchi function tuzing 
+
+// (capitalizeWords("javascript is fun")); // Output: JavaScript Is Fun
+// gap ichida hamma sozlarning  bosh harfi katta qilib chiqarish
+
+// let str="JavaScript Developer's Guide" => 4
+// string ichida  nechta katta harf qatnashganini topib beruvchi function tuzing
+
